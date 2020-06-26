811e8910cd7f1b65e2bb2048e0f02f5740137350
cozej4 <ilakozejumanne@gmail.com>
2020-06-26 18:25:41 +0300

    Updated title label of base tb community followup register fragment

--- opensrp-chw-tb/src/main/java/org/smartregister/chw/tb/fragment/BaseTbCommunityFollowupRegisterFragment.kt
+++ opensrp-chw-tb/src/main/java/org/smartregister/chw/tb/fragment/BaseTbCommunityFollowupRegisterFragment.kt
@@ -1,11 +1,24 @@
 package org.smartregister.chw.tb.fragment
 
 
+import org.smartregister.chw.tb.R
 import org.smartregister.chw.tb.model.BaseTbCommunityFollowupModel
 import org.smartregister.chw.tb.presenter.BaseTbCommunityFollowupPresenter
 import org.smartregister.commonregistry.CommonPersonObjectClient
+import org.smartregister.view.customcontrols.CustomFontTextView
+import org.smartregister.view.customcontrols.FontVariant
 
 open class BaseTbCommunityFollowupRegisterFragment : BaseTbRegisterFragment() {
+
+    override fun setupViews(view: android.view.View) {
+        super.setupViews(view)
+        view.findViewById<CustomFontTextView>(R.id.txt_title_label)?.apply {
+            visibility = android.view.View.VISIBLE
+            text = getString(R.string.tb_community_followup_clients)
+            setFontVariant(FontVariant.REGULAR)
+        }
+    }
+
     override fun initializePresenter() {
         if (activity == null) {
             return
