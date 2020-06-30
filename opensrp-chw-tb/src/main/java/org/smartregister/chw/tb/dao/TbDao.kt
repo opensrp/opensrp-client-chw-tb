89029d51904605fb8339abf0f261b2f302754d46
cozej4 <ilakozejumanne@gmail.com>
2020-06-30 12:16:41 +0300

    Refactored the query used to check if the client is registered for tb

--- opensrp-chw-tb/src/main/java/org/smartregister/chw/tb/dao/TbDao.kt
+++ opensrp-chw-tb/src/main/java/org/smartregister/chw/tb/dao/TbDao.kt
@@ -116,7 +116,7 @@ object TbDao : AbstractDao() {
             """select count(ec_tb_register.base_entity_id) count
                from ec_tb_register
                where base_entity_id = '${baseEntityID}'
-               and tb_case_closure_date IS NULL and
+               and tb_case_closure_date IS NULL
                and ec_tb_register.is_closed = 0
             """
 
