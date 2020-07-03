5705c701eb511ba833689004259409ee43e3663b
cozej4 <ilakozejumanne@gmail.com>
2020-07-03 08:18:35 +0300

    refactored obtaining of tb members by member id in cases of the query obtaining more than one entry

--- opensrp-chw-tb/src/main/java/org/smartregister/chw/tb/dao/TbDao.kt
+++ opensrp-chw-tb/src/main/java/org/smartregister/chw/tb/dao/TbDao.kt
@@ -172,7 +172,7 @@ object TbDao : AbstractDao() {
         var res = readData(sql, dataMap)
         if (res == null)
             res = readData(sqlHf, dataMap)
-        return if (res == null || res.size != 1) null else res[0]
+        return if (res == null || res.size == 0) null else res[0]
     }
 
     @JvmStatic
@@ -191,10 +191,11 @@ object TbDao : AbstractDao() {
                     inner join ec_tb_community_followup mr on mr.entity_id = m.base_entity_id 
                     left join ec_family_member fh on fh.base_entity_id = f.family_head 
                     left join ec_family_member pcg on pcg.base_entity_id = f.primary_caregiver 
-                    where mr.base_entity_id ='${baseEntityID}' OR mr.entity_id ='${baseEntityID}' """
+                    where mr.base_entity_id ='${baseEntityID}' OR mr.entity_id ='${baseEntityID}' AND
+                    mr.base_entity_id NOT IN (SELECT community_referral_form_id FROM ec_tb_community_feedback)"""
 
         val res = readData(sql, dataMap)
-        return if (res == null || res.size != 1) null else res[0]
+        return if (res == null || res.size == 0) null else res[0]
     }
 
     @JvmStatic
