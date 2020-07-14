de7ce068773444a0965b91e09f57028176fbb062
cozej4 <ilakozejumanne@gmail.com>
2020-07-14 15:53:00 +0300

    Fixed a bug in launching sample app

--- sample/src/main/java/org/smartregister/chw/tb/sample/activity/TbFormRegistrationActivity.kt
+++ sample/src/main/java/org/smartregister/chw/tb/sample/activity/TbFormRegistrationActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.nerdstone.neatformcore.form.json.JsonFormBuilder
+import com.nerdstone.neatformcore.form.json.JsonFormEmbedded
 import org.smartregister.chw.tb.R
 
 class TbFormRegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.neat_tb_registration_form_activity)
-        JsonFormBuilder(
-            this, "json.form/tb_registration_form.json",
-            findViewById<LinearLayout>(R.id.formLayout)
-        ).buildForm(null, null)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.sampleToolBar)
+        setSupportActionBar(toolbar)
+        val jsonFormBuilder = JsonFormBuilder(this, "json.form/tb_followup_visit.json")
+        JsonFormEmbedded(jsonFormBuilder, findViewById<LinearLayout>(R.id.formLayout)).buildForm()
     }
 }
